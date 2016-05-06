372f57fcbccdac3fcdc5dd774b050edc17ec26b4
K Developer <kajigga@gmail.com>
2016-05-06 08:57:06 -0600

    more bacon

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ def baconIpsumFetch(*args,**kwargs):
     
 
     #print 'with_bacon', with_bacon
-    if with_bacon == 'yes':
+    if with_bacon == 'no':
       # Now get the bacon ipsum
       bacon_url = "http://baconipsum.com/api/?type=meat-and-filler&paras=%d&start-with-lorem=0" % num_para 
 
